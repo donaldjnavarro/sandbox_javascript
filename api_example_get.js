e5a4d4a5a92ffd104d7e4df0cfd121b2adcbd39d
donaldjnavarro <donaldjnavarro@gmail.com>
2020-11-01 16:56:54 -0500

    basic api calls

--- api_example_get.js
+++ api_example_get.js
@@ -1,20 +1,29 @@
+// example api get request based on https://nodejs.dev/learn/making-http-requests-with-nodejs
 const https = require('https')
+
+// api target parameters
 const options = {
-  hostname: 'example.com',
+  hostname: 'exampledes.com',
   port: 443,
   path: '/todos',
   method: 'GET'
 }
 
+// api request execution
 const req = https.request(options, res => {
+  // Display the status code
   console.log(`statusCode: ${res.statusCode}`)
 
+  // Display the returned data
   res.on('data', d => {
     process.stdout.write(d)
   })
 })
 
 req.on('error', error => {
+  console.log()
+  console.log("ERROR")
+  console.log()
   console.error(error)
 })
 
