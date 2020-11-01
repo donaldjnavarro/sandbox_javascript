d244bb5e5d0499e83026c1cd1070a1228f237fed
donaldjnavarro <donaldjnavarro@gmail.com>
2020-11-01 17:35:32 -0500

    expanding class functions

--- example_class.js
+++ example_class.js
@@ -4,14 +4,33 @@ class Car {
         this.name = name;
         this.year = year;
     }
-
     // method that returns the car's age based on its year compared to the current year
     age() {
         let date = new Date();
         return date.getFullYear() - this.year;
     }
+    // Display age in human readable format
+    displayAge() {
+        console.log("This car is " + this.age() + " years old.");
+    }
+    // Display the data within a specified key in the current instance
+    displayKey(keyName) {
+        // Only act when the key exists
+        if (this.hasOwnProperty(keyName)) {
+            console.log(keyName+": "+this[keyName])
+        }
+    }
+    // Display every key and value in the current instance
+    displayAll() {
+        for (var i = 0; i < Object.getOwnPropertyNames(this).length; i++) {
+            this.displayKey(Object.getOwnPropertyNames(this)[i])
+        }
+    }
+    
 }
 
 // create an example class instance
 let myCar = new Car("Ford", 2014);
-console.log("My car is " + myCar.age() + " years old.");
+
+myCar.displayAll()
+myCar.displayAge()
